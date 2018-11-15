01b4be44d197bf3589a3c4c803ec00431c714f57
JackKeet <37407728+JackKeet@users.noreply.github.com>
2018-11-15 19:17:18 +0000

    1.4.py
    Both API's implemented

--- v1.1.py
+++ v1.1.py
@@ -6,34 +6,48 @@ import requests
 
 
 
-
 ###########################################################################
 
 window = Tk()
-window.geometry("350x350")
+window.geometry("450x450")
 
 window.title('Movie Finder')
 
 label = Label(window,text = 'Please enter a movie to search for:')
 label.pack(padx = 10, pady = 50)
 
-
-def search_movie(title):
+def OMDB_Search(title):
     key = 'c5f36360'
     serviceurl = 'http://www.omdbapi.com/?'
     apikey = '&apikey='+key
-    
+
     url = serviceurl + apikey + '&s=' + title
     json_data = requests.get(url).json()
     list = []
 
     for item in json_data['Search']:
-        
+
         list.append("Movie: " + item['Title'] + "\n" + "Released: " + item['Year'])
 
     return list
 
-def result():
+
+def TMDB_Search(title):
+    key = '399f6191c9956c7ac44da18aa4461125'
+    serviceurl = 'https://api.themoviedb.org/3/search/movie?'
+    apikey = 'apikey='+key
+
+    url = 'https://api.themoviedb.org/3/search/movie?api_key=399f6191c9956c7ac44da18aa4461125&query=' + title
+    json_data = requests.get(url).json()
+    list = []
+
+    for item in json_data['results']:
+
+        list.append("Movie: " + item['title'] + "\n" + "Released: " + item['release_date'])
+
+    return list
+
+def result1():
 
     title = searchBox.get()
     newwin = tk.Tk()
@@ -41,37 +55,71 @@ def result():
 
     T = Text(newwin,height=2, width=30)
     T.pack()
-    T.insert(END, search_movie(title)[0])
-    btn_addF1 = Button(newwin, text = 'Add to favourites', command=fav(search_movie(title)[0]))
+    T.insert(END, OMDB_Search(title)[0])
+    btn_addF1 = Button(newwin, text = 'Add to favourites', command=fav(OMDB_Search(title)[0]))
     btn_addF1.pack()
 
     T1 = Text(newwin,height=2, width=30)
     T1.pack()
-    T1.insert(END, search_movie(title)[1])
-    btn_addF2 = Button(newwin, text = 'Add to favourites', command=fav(search_movie(title)[1]))
+    T1.insert(END, OMDB_Search(title)[1])
+    btn_addF2 = Button(newwin, text = 'Add to favourites', command=fav(OMDB_Search(title)[1]))
     btn_addF2.pack()
-    
+
     T2 = Text(newwin,height=2, width=30)
     T2.pack()
-    T2.insert(END, search_movie(title)[2])
-    btn_addF3 = Button(newwin, text = 'Add to favourites', command=fav(search_movie(title)[2]))
+    T2.insert(END, OMDB_Search(title)[2])
+    btn_addF3 = Button(newwin, text = 'Add to favourites', command=fav(OMDB_Search(title)[2]))
     btn_addF3.pack()
 
     T3 = Text(newwin,height=2, width=30)
     T3.pack()
-    T3.insert(END, search_movie(title)[3])
-    btn_addF4 = Button(newwin, text = 'Add to favourites', command=fav(search_movie(title)[3]))
+    T3.insert(END, OMDB_Search(title)[3])
+    btn_addF4 = Button(newwin, text = 'Add to favourites', command=fav(OMDB_Search(title)[3]))
     btn_addF4.pack()
 
     T4 = Text(newwin,height=2, width=30)
     T4.pack()
-    T4.insert(END, search_movie(title)[4])
-    btn_addF5 = Button(newwin, text = 'Add to favourites', command=fav(search_movie(title)[4]))
+    T4.insert(END, OMDB_Search(title)[4])
+    btn_addF5 = Button(newwin, text = 'Add to favourites', command=fav(OMDB_Search(title)[4]))
+    btn_addF5.pack()
+
+def result2():
+
+    title = searchBox.get()
+    newwin = tk.Tk()
+    newwin.geometry("400x400")
+
+    T = Text(newwin,height=2, width=30)
+    T.pack()
+    T.insert(END, TMDB_Search(title)[0])
+    btn_addF1 = Button(newwin, text = 'Add to favourites', command=fav(TMDB_Search(title)[0]))
+    btn_addF1.pack()
+
+    T1 = Text(newwin,height=2, width=30)
+    T1.pack()
+    T1.insert(END, TMDB_Search(title)[1])
+    btn_addF2 = Button(newwin, text = 'Add to favourites', command=fav(TMDB_Search(title)[1]))
+    btn_addF2.pack()
+
+    T2 = Text(newwin,height=2, width=30)
+    T2.pack()
+    T2.insert(END, TMDB_Search(title)[2])
+    btn_addF3 = Button(newwin, text = 'Add to favourites', command=fav(TMDB_Search(title)[2]))
+    btn_addF3.pack()
+
+    T3 = Text(newwin,height=2, width=30)
+    T3.pack()
+    T3.insert(END, TMDB_Search(title)[3])
+    btn_addF4 = Button(newwin, text = 'Add to favourites', command=fav(TMDB_Search(title)[3]))
+    btn_addF4.pack()
+
+    T4 = Text(newwin,height=2, width=30)
+    T4.pack()
+    T4.insert(END, TMDB_Search(title)[4])
+    btn_addF5 = Button(newwin, text = 'Add to favourites', command=fav(TMDB_Search(title)[4]))
     btn_addF5.pack()
 
 def fav(movie_to_add):
-    
-            
 
     f= open("fav.txt","a+")
     f.write(movie_to_add + '\n')
@@ -87,7 +135,7 @@ def favourites():
     f=open("fav.txt", "r")
     if f.mode == 'r':
         contents = f.read()
-        
+
     T = Text(newwin2,height=20, width=30)
     T.pack()
     T.insert(END, contents)
@@ -100,9 +148,13 @@ searchBox.delete(0, END)
 searchBox.insert(0, "")
 
 
-#Search button
-btn_search = Button(window, text = 'Search', command=result)
+#Search button for OMDB
+btn_search = Button(window, text = 'Search OMDB', command=result1)
 btn_search.pack(padx = 120, pady = 20)
+
+#Searcg Button for TMDB
+btn_search = Button(window, text = 'Search TMDB', command=result2)
+btn_search.pack(padx = 120, pady = 30)
 
 #Favourites
 btn_search = Button(window, text = 'Favourites', command=favourites)
