f7f8e3cac2bd3569a50128c6123f067c5e3c4bcd
JackKeet <37407728+JackKeet@users.noreply.github.com>
2018-11-15 13:16:02 +0000

    1.2
    Search 5 results + start of favorites

--- v1.1.py
+++ v1.1.py
@@ -34,13 +34,79 @@ def search_movie(title): #"Function" to search for movies
 
 def result():
 
-    title = searchBox.get() #Gets the input from the search box
-    newwin = tk.Tk() #Creates singular box to put data in | Possibly put this in search function to make several boxes instead | Need to figure out how to do variable(1),variable(2)... etc|
-    newwin.geometry("400x400") #Size of box
-    T = Text(newwin) #Looking for text to put in box
-    T.pack() 
-    T.insert(END, search_movie(title)) # Puts text grabbed from search in search_movie fucntion |
+    title = searchBox.get()
+    newwin = tk.Tk()
+    newwin.geometry("400x400")
+    T[]
+    T = Text(newwin,height=2, width=30)
+    T.pack()
+    T.insert(END, search_movie(title)[0])
+    btn_addF1 = Button(newwin, text = 'Add to favourites', command='fav')
+    btn_addF1.pack()
 
+    T1 = Text(newwin,height=2, width=30)
+    T1.pack()
+    T1.insert(END, search_movie(title)[1])
+    btn_addF2 = Button(newwin, text = 'Add to favourites', command='fav')
+    btn_addF2.pack()
+    
+    T2 = Text(newwin,height=2, width=30)
+    T2.pack()
+    T2.insert(END, search_movie(title)[2])
+    btn_addF3 = Button(newwin, text = 'Add to favourites', command='fav')
+    btn_addF3.pack()
+
+    T3 = Text(newwin,height=2, width=30)
+    T3.pack()
+    T3.insert(END, search_movie(title)[3])
+    btn_addF4 = Button(newwin, text = 'Add to favourites', command='fav')
+    btn_addF4.pack()
+
+    T4 = Text(newwin,height=2, width=30)
+    T4.pack()
+    T4.insert(END, search_movie(title)[4])
+    btn_addF5 = Button(newwin, text = 'Add to favourites', command='fav')
+    btn_addF5.pack()
+    
+def fav():
+        favList = []
+
+        if btn_addF1 == true:
+            favList.append(T)
+
+        if btn_addF2 == true:
+            favList.append(T1)
+
+        if btn_addF3 == true:
+            favList.append(T2)
+            
+        if btn_addF4 == true:
+            favList.append(T3)
+            
+        if btn_addF5 == true:
+            favList.append(T4)
+            
+
+        f= open("fav.txt","a+")
+        f.write(favList)
+        f.close()
+
+
+def favourites():
+
+    title = searchBox.get()
+    newwin2 = tk.Tk()
+    newwin2.geometry("400x400")
+
+    f=open("fav.txt", "r")
+    if f.mode == 'r':
+        contents = f.read()
+        
+    T = Text(newwin2,height=20, width=30)
+    T.pack()
+    T.insert(END, contents)
+
+    
 #Search box
 searchBox = Entry(window)
 searchBox.pack()
