991faea9e027eb960d1739ac691e412b18a1dea6
Stevie Mallyon <36130859+stevie333@users.noreply.github.com>
2018-11-17 12:43:53 +0000

    Update Movie_Search.py
    Added ability to edit the wish list. You then push the update list button which saves the updates to the text file.
    Backgrounds and formatting added.

--- Movie_Search.py
+++ Movie_Search.py
@@ -9,13 +9,14 @@ from tkinter import messagebox
 ###########################################################################
 
 window = Tk()
-window.geometry("450x450")
+window.geometry("530x530")
+window.configure(background='palegreen4')
 
 window.title('Movie Finder')
 
 list = []
 
-label = Label(window,text = 'Please enter a movie to search for:')
+label = Label(window,text = 'Please enter a movie to search for:', background='palegreen4')
 label.pack(padx = 10, pady = 50)
 
 def OMDB_Search(title):
@@ -53,38 +54,56 @@ def result1():
 
     title = searchBox.get()
     newwin = tk.Tk()
-    newwin.geometry("400x400")
-    
+    newwin.geometry("500x500")
+    newwin.configure(background='palegreen4')
     try:
-        T = Text(newwin,height=2, width=30)
+        T = Text(newwin,height=2, width=40)
         T.pack()
         T.insert(END, OMDB_Search(title)[0])
         btn_addF1 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(OMDB_Search(title)[0]))
         btn_addF1.pack()
 
-        T1 = Text(newwin,height=2, width=30)
+        T1 = Text(newwin,height=2, width=40)
         T1.pack()
         T1.insert(END, OMDB_Search(title)[1])
         btn_addF2 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(OMDB_Search(title)[1]))
         btn_addF2.pack()
 
-        T2 = Text(newwin,height=2, width=30)
+        T2 = Text(newwin,height=2, width=40)
         T2.pack()
         T2.insert(END, OMDB_Search(title)[2])
         btn_addF3 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(OMDB_Search(title)[2]))
         btn_addF3.pack()
 
-        T3 = Text(newwin,height=2, width=30)
+        T3 = Text(newwin,height=2, width=40)
         T3.pack()
         T3.insert(END, OMDB_Search(title)[3])
         btn_addF4 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(OMDB_Search(title)[3]))
         btn_addF4.pack()
 
-        T4 = Text(newwin,height=2, width=30)
+        T4 = Text(newwin,height=2, width=40)
         T4.pack()
         T4.insert(END, OMDB_Search(title)[4])
         btn_addF5 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(OMDB_Search(title)[4]))
         btn_addF5.pack()
+
+        T5 = Text(newwin,height=2, width=40)
+        T5.pack()
+        T5.insert(END, OMDB_Search(title)[5])
+        btn_addF6 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(OMDB_Search(title)[5]))
+        btn_addF6.pack()
+
+        T6 = Text(newwin,height=2, width=40)
+        T6.pack()
+        T6.insert(END, OMDB_Search(title)[6])
+        btn_addF7 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(OMDB_Search(title)[6]))
+        btn_addF7.pack()
+
+        T7 = Text(newwin,height=2, width=40)
+        T7.pack()
+        T7.insert(END, OMDB_Search(title)[7])
+        btn_addF8 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(OMDB_Search(title)[7]))
+        btn_addF8.pack()
         
     except:
         messagebox.showerror("error", "Movie Not Found")
@@ -93,40 +112,59 @@ def result2():
 
     title = searchBox.get()
     newwin = tk.Tk()
-    newwin.geometry("400x400")
+    newwin.geometry("500x500")
+    newwin.configure(background='palegreen4')
 
     
     try:
-        T = Text(newwin,height=2, width=30)
+        T = Text(newwin,height=2, width=40)
         T.pack()
         T.insert(END, TMDB_Search(title)[0])
         btn_addF1 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(TMDB_Search(title)[0]))
         btn_addF1.pack()
 
-        T1 = Text(newwin,height=2, width=30)
+        T1 = Text(newwin,height=2, width=40)
         T1.pack()
         T1.insert(END, TMDB_Search(title)[1])
         btn_addF2 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(TMDB_Search(title)[1]))
         btn_addF2.pack()
 
-        T2 = Text(newwin,height=2, width=30)
+        T2 = Text(newwin,height=2, width=40)
         T2.pack()
         T2.insert(END, TMDB_Search(title)[2])
         btn_addF3 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(TMDB_Search(title)[2]))
         btn_addF3.pack()
 
-        T3 = Text(newwin,height=2, width=30)
+        T3 = Text(newwin,height=2, width=40)
         T3.pack()
         T3.insert(END, TMDB_Search(title)[3])
         btn_addF4 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(TMDB_Search(title)[3]))
         btn_addF4.pack()
 
-        T4 = Text(newwin,height=2, width=30)
+        T4 = Text(newwin,height=2, width=40)
         T4.pack()
         T4.insert(END, TMDB_Search(title)[4])
         btn_addF5 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(TMDB_Search(title)[4]))
         btn_addF5.pack()
+        
+        T5 = Text(newwin,height=2, width=40)
+        T5.pack()
+        T5.insert(END, TMDB_Search(title)[5])
+        btn_addF6 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(TMDB_Search(title)[5]))
+        btn_addF6.pack()
 
+        T6 = Text(newwin,height=2, width=40)
+        T6.pack()
+        T6.insert(END, TMDB_Search(title)[6])
+        btn_addF7 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(TMDB_Search(title)[6]))
+        btn_addF7.pack()
+
+        T7 = Text(newwin,height=2, width=40)
+        T7.pack()
+        T7.insert(END, TMDB_Search(title)[7])
+        btn_addF8 = Button(newwin, text = 'Add to wishlist', command=lambda : wish(TMDB_Search(title)[7]))
+        btn_addF8.pack()
+        
         
     except:
         messagebox.showerror("error", "Movie Not Found")
@@ -143,38 +181,54 @@ def wish_list():
 
     title = searchBox.get()
     newwin2 = tk.Tk()
-    newwin2.geometry("400x400")
+    newwin2.geometry("700x650")
+    newwin2.configure(background='palegreen4')
+    newwin2.title("Wish List")
+    
+    label = Label(newwin2,text = 'Welcome to your wish list!\n\nYou can edit the list yourself here by highlighting the movie followed by the backspace key on your keyboard.\n\n Do not forget to press update!', background='palegreen4')
+    label.pack(padx = 10, pady = 50)
+                  
+    f = open("wish.txt", "r")
 
-    f=open("wish.txt", "r")
-    if f.mode == 'r':
-        contents = f.read()
+    contents = f.read()
 
     T = Text(newwin2,height=20, width=30)
-    T.pack()
+    T.pack(padx = 30, pady = 50)
     T.insert(END, contents)
+
+    sub = Button(newwin2, text = 'Update List', command=lambda : update_wlist(T.get('1.0', 'end-1c')))
+    sub.pack()
+
+    f.close()
+
+def update_wlist(change):
+    f = open("wish.txt","a+")
+    f.truncate(0)
+    f.write(change)
+    messagebox.showinfo("Success", "Your wish list has been updated!")
 
 
 #Search box
-searchBox = Entry(window)
+searchBox = Entry(window, bd = 4, width=30)
 searchBox.pack()
 searchBox.delete(0, END)
 searchBox.insert(0, "")
-
+searchBox.pack(padx = 50, pady = 30)
 
 #Search button for OMDB
-btn_search = Button(window, text = 'Search OMDB', command=result1)
-btn_search.pack(padx = 120, pady = 20)
+btn_search = Button(window, height = 3 ,width = 20, text = 'Search OMDB', command=result1)
+btn_search.pack(padx = 130, pady = 5)
 
 #Search Button for TMDB
-btn_search = Button(window, text = 'Search TMDB', command=result2)
-btn_search.pack(padx = 120, pady = 30)
+btn_search = Button(window, height = 3 ,width = 20, text = 'Search TMDB', command=result2)
+btn_search.pack(padx = 135, pady = 5)
 
 #Wish list
-btn_search = Button(window, text = 'Wish List', command=wish_list)
-btn_search.pack(padx = 135, pady = 20)
+btn_search = Button(window, height = 3 ,width = 20, text = 'Wish List', command=wish_list)
+btn_search.pack(padx = 140, pady = 5)
 
 #Exit button
-btn_end = Button(window, text = 'Exit', command=exit)
-btn_end.pack(padx = 150, pady = 20)
+btn_end = Button(window, height = 3 ,width = 20, text = 'Exit', command=exit)
+btn_end.pack(padx = 145, pady = 5)
 
 window.mainloop()
